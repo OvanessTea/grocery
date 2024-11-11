640e81460593e96ce41a39a07679ea70fb653fa7
OvanessTea <OvanessTea@yandex.com>
2024-11-11 02:29:47 +0100

    add userModel

---
+++ server/model/userModel.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }, {timestamps: true}, )
+    return User;
+}
